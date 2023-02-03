b8b720ce45e84544a210f717f11bd18b7a4e1e0c
Mukesh Yadav <mukeshyadavparty55@gmail.com>
2023-02-04 02:11:36 +0530

    final dice roll app

--- app/src/main/java/com/mukesh/diceroller/MainActivity.kt
+++ app/src/main/java/com/mukesh/diceroller/MainActivity.kt
@@ -1,28 +1,30 @@
 package com.mukesh.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mukesh.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //it means we will assign the value of var later
     //here var is used means it can be reinitialised later
-    private lateinit var ImageView: ImageView
+//    private lateinit var ImageView: ImageView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
        //val is the final variable means we have to initialise it right away
-        val btn = findViewById<Button>(R.id.rollBtn);
-        ImageView = findViewById(R.id.dice)
+//        val btn = findViewById<Button>(R.id.rollBtn);
+//        ImageView = findViewById(R.id.dice)
 
 
-        btn.setOnClickListener { rollDice() }
+//        btn.setOnClickListener { rollDice() }
+        binding.rollBtn.setOnClickListener{rollDice()}
     }
 //    The (1..6) syntax creates an integer range from 1 to 6.
 //    The random() function is called on this range to generate a random number within that range.
@@ -39,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         5->R.drawable.dice_5
         else->R.drawable.dice_6
     }
-    ImageView.setImageResource(drawableImage);
+//    ImageView.setImageResource(drawableImage);
+    binding.dice.setImageResource(drawableImage);
+
+    //syntax
+    //binding.id.xyx
+
     }
 }
